da63e23ea817c1dcab2b0fce177ce068ce34699c
Javier Ramirez <44209276+javier-cr@users.noreply.github.com>
2019-06-27 00:36:18 -0700

    Change files to PHP

---
+++ index.php
@@ -0,0 +1,197 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="robots" content="index, follow">
+    <meta name="description"
+        content="Javi's Coffee sources only the highest quality coffees and teas. Learn more about our delicious drinks today." />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="./assets/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <link rel="apple-touch-icon" sizes="180x180" href="./assets/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./assets/favicon-16x16.png">
+    <link rel="manifest" href="./assets/site.webmanifest">
+    <link rel="mask-icon" href="./assets/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="./assets/favicon.ico">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./bootstrap.min.css">
+    <link rel="stylesheet" href="./customjc.css">
+    <title>Javi's Coffee: Delicious Coffee for All</title>
+</head>
+
+<body>
+
+    <!--Alert Message-->
+    <div class="alert alert-critical text-white border-0 rounded-0 m-0" role="alert">
+        <a href="./shop/" class="text-white">Our Purple Rain roast is on sale! Order now before it's all gone! Ends
+            6/7 at 9pm
+            (PT).</a>
+    </div>
+
+    <!--Nav Bar-->
+    <nav class="navbar navbar-expand-sm navbar-light bg-white">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="./">
+                <img src="./assets/logo.png" height="55px" alt="Javi's Coffee logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav nav-fill w-75 mx-4 text-uppercase">
+                    <a class="nav-item nav-link" href="./shop/">Shop</a>
+                    <a class="nav-item nav-link disabled" href="#">Stores</a>
+                    <a class="nav-item nav-link" href="./ourstory/">Our Story</a>
+                    <a class="nav-item nav-link disabled" href="#">Javi's Rewards</a>
+                    <a class="nav-item nav-link" href="./contact/">Contact</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <!--Hero Image-->
+    <div id="header" class="container-fluid">
+        <div class="overlay"></div>
+        <img src="./assets/home_hero_store.jpg" class="img-responsive" alt="Image of a bright, open coffee shop">
+        <div class="container h-100 pb-5">
+            <div class="d-flex h-100 text-center align-items-center pb-5">
+                <div class="w-100 pb-5">
+                    <h1 class="hero-title text-white pb">Delicious Craft Coffees</h1>
+                    <h3 class="text-white pb-3">Made fresh, just for you</h3>
+                    <button type="button" class="btn btn-danger">
+                        <a href="./shop/" style="color:inherit">Shop Now</a>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Rewards-->
+    <div class="col-md-10 mx-auto my-2 my-sm-5 py-md-3 py-lg-5">
+        <div class="row mx-1 m-md-3">
+            <div id="rewards-logo" class="col-8 mx-auto my-sm-auto mt-3 mb-2 col-sm-3">
+                <img src="./assets/rewards_logo.png" class="mw-100" alt="Rewards program logo">
+            </div>
+
+            <div class="col-sm-9">
+                <h2 class="rewards-title text-sm-left text-lowercase text-dark font-weight-bold">Make Every Sip More
+                    Rewarding
+                </h2>
+                <p class="text-sm-left text-dark">Say hello to rewards everywhere you go. At Javi's Coffee stores,
+                    grocery markets, and online, collect High-Fives&trade; every step of the way.</p>
+                <p class="text-sm-left">
+                    <button type="button" class="btn btn-danger" disabled>
+                        <a href="./rewards/" style="color:inherit">Learn more</a>
+                    </button>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <!--Shop-->
+    <div class="col py-5 bg-light">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="shop-title text-lowercase">Shop Javi's</h2>
+            </div>
+
+            <div class="card-deck col-lg-9 mx-auto">
+
+                <div class="card">
+                    <img class="card-img-top" src="./assets/home_shop_cap.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <a href="./shop/" class="card-title btn btn-outline-danger stretched-link">Shop Coffee</a>
+                        <p class="card-text">Hand-roasted coffee, delivered fresh for ultimate flavor.</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img class="card-img-top" src="./assets/home_shop_tea.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <a href="./shop/" class="card-title btn btn-outline-danger stretched-link">Shop Tea</a>
+                        <p class="card-text">Our finest selection of premium whole leaf tea.</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img class="card-img-top" src="./assets/home_shop_cap.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <a href="./shop/" class="card-title btn btn-outline-danger stretched-link">Shop Accessories</a>
+                        <p class="card-text">Brewers and accessories for making the perfect cup at home.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <footer class="page-footer bg-dark font-small p-5">
+        <div class="container-fluid text-center text-md-left">
+            <div class="row">
+
+                <div class="col-md-6 mt-md-0 mt-3">
+                    <h5 class="text-uppercase text-light">Javi's Coffee</h5>
+                    <p class="text-light">At Javi’s Coffee, we work to make sure that each cup of our
+                        delicious coffee is served with joy. We carefully select our
+                        coffees for their bold, full-bodied flavor and with attention
+                        to ethical and sustainable practices. We invite you to indulge
+                        in one of our thirteen signature roasts. Come share in our
+                        mission to make our community and the world a better place.</p>
+                </div>
+
+                <hr class="clearfix w-100 d-md-none pb-3">
+
+                <div class="col-md-3 mb-md-0 mb-3">
+                    <h5 class="text-uppercase text-white">Shop</h5>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="./shop/" class="text-light">Coffee</a>
+                        </li>
+                        <li>
+                            <a href="./shop/" class="text-light">Tea</a>
+                        </li>
+                        <li>
+                            <a href="./shop/" class="text-light">Accessories</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-md-3 mb-md-0 mb-3">
+                    <h5 class="text-uppercase text-white">About Us</h5>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="./ourstory/" class="text-light">Our Story</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-light">Javi's Rewards</a>
+                        </li>
+                        <li>
+                            <a href="./contact/" class="text-light">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+        <!-- Footer Links -->
+        <div class="footer-copyright text-center text-white pt-4">© 2019 Javi's Coffee</div>
+
+    </footer>
+    <!-- Footer -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="./jquery-3.3.1.slim.min.js"></script>
+    <script src="./popper.min.js"></script>
+    <script src="./bootstrap.min.js"></script>
+</body>
+
+</html>
