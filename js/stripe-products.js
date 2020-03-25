5b3e0cb3d1ff59cbb142e0227903e822d71c2361
Javier Ramirez <44209276+javier-cr@users.noreply.github.com>
2020-03-25 10:17:49 -0700

    Streamline Stripe JS

--- js/stripe-products.js
+++ js/stripe-products.js
@@ -9,45 +9,9 @@ checkoutButtonPR.addEventListener('click', function () {
     // When the customer clicks on the button, redirect
     // them to Checkout.
     stripe.redirectToCheckout({
-        items: [{ sku: 'sku_Ex9Mj2e4o7Cjpa', quantity: 1 }],
-
-        successUrl: "https://javiscoffee.com/shop?order=success",
-        cancelUrl: "https://javiscoffee.com/shop/",
-    })
-        .then(function (result) {
-            if (result.error) {
-                // If `redirectToCheckout` fails due to a browser or network
-                // error, display the localized error message to your customer.
-                var displayError = document.getElementById('error-message');
-                displayError.textContent = result.error.message;
-            }
-        });
-});
-
-checkoutButtonAE.addEventListener('click', function () {
-    // When the customer clicks on the button, redirect
-    // them to Checkout.
-    stripe.redirectToCheckout({
-        items: [{ sku: 'sku_FBS4G1aRNUh4dA', quantity: 1 }],
-
-        successUrl: "https://javiscoffee.com/shop?order=success",
-        cancelUrl: "https://javiscoffee.com/shop/",
-    })
-        .then(function (result) {
-            if (result.error) {
-                // If `redirectToCheckout` fails due to a browser or network
-                // error, display the localized error message to your customer.
-                var displayError = document.getElementById('error-message');
-                displayError.textContent = result.error.message;
-            }
-        });
-});
-
-checkoutButtonEI.addEventListener('click', function () {
-    // When the customer clicks on the button, redirect
-    // them to Checkout.
-    stripe.redirectToCheckout({
-        items: [{ sku: 'sku_FBS5NDx8V1UhOi', quantity: 1 }],
+        items: [{ sku: 'sku_Ex9Mj2e4o7Cjpa', quantity: 1 },
+                { sku: 'sku_FBS4G1aRNUh4dA', quantity: 1 },
+                { sku: 'sku_FBS5NDx8V1UhOi', quantity: 1 } ],
 
         successUrl: "https://javiscoffee.com/shop?order=success",
         cancelUrl: "https://javiscoffee.com/shop/",
