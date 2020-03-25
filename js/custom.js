4bfd793927c12be304253df23eff36c866c070f2
Javier Ramirez <44209276+javier-cr@users.noreply.github.com>
2020-03-24 21:51:34 -0700

    Fix Stripe error modal

--- js/custom.js
+++ js/custom.js
@@ -21,3 +21,9 @@ var url = window.location.href;
         if (url.indexOf('?order=success') != -1) {
             $('#orderSuccess').modal('show');
         }
+
+//If url matches pattern, show order success modal
+var url = window.location.href;
+        if (url.indexOf('?order=fail') != -1) {
+            $('#orderFail').modal('show');
+        }
